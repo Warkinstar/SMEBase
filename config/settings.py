96057d2d74ad2501e6d607de573eaaaa45431f04
Ramil <Warkinstar@mail.ru>
2023-04-01 18:12:36 +0600

    enterprises application

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # My apps
     "accounts.apps.AccountsConfig",
+    "enterprises.apps.EnterprisesConfig",
 ]
 
 MIDDLEWARE = [
