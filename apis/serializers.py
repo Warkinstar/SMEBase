87d515065f2183afb849546e700e4dd8f0a27be7
Ramil <Warkinstar@mail.ru>
2023-07-05 22:07:52 +0600

    User & Owner Business Type serializers

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
-from enterprises.models import Company
+from enterprises.models import Company, OwnershipType, BusinessType
+from django.contrib.auth import get_user_model
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "first_name", "last_name", "email", "phone_number"]
+
+class OwnershipTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OwnershipType
+        fields = ["name"]
+
+
+class BusinessTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusinessType
+        fields = ["name"]
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    ownership_type = OwnershipTypeSerializer()
+    business_type = BusinessTypeSerializer()
+
     class Meta:
         model = Company
         fields = "__all__"
