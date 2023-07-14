5023d3de31b851844dcf6291874c33cde4e0f6af
Ramil <Warkinstar@mail.ru>
2023-07-14 21:50:20 +0600

    Redoc & Swagger-ui

--- config/urls.py
+++ config/urls.py
@@ -17,13 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("api/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path(
+        "api/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")
+    ),
     path("api/v1/", include("apis.urls", namespace="api_v1")),  # API version 1
     path("api-auth/", include("rest_framework.urls")),  # auth urls for api
     path("accounts/", include("accounts.urls")),
@@ -31,6 +37,16 @@ urlpatterns = [
     path("enterprises/", include("enterprises.urls")),
     path("products/", include("products.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
 
 if settings.DEBUG:
